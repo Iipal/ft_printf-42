d9d5e343240e2f40e21e91265a633c011a4d0286
Iipal <malkoleyplay@gmail.com>
2019-03-10 09:14:51 +0200

    improved %d output data typed

--- srcs/pf_output_decimal.c
+++ srcs/pf_output_decimal.c
@@ -6,16 +6,16 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/09 15:56:35 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/03/09 23:52:58 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/03/10 09:06:50 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static void		add_is_minus_flag_output(t_printf *p, int out_len,
-										int out, string out_str)
+static void	add_is_minus_flag_output(t_printf *p, long out_len,
+										long out, string out_str)
 {
-	int	i;
+	long	i;
 
 	i = out_len - 1;
 	if (p->flags[1] && out >= 0)
@@ -30,10 +30,10 @@ static void		add_is_minus_flag_output(t_printf *p, int out_len,
 		p->flags[2] ? _PUT('0') : _PUT(' ');
 }
 
-static void		add_no_minus_flag_output(t_printf *p, int out_len,
-										int out, string out_str)
+static void	add_no_minus_flag_output(t_printf *p, long out_len,
+										long out, string out_str)
 {
-	int	i;
+	long	i;
 
 	i = -1;
 	while (++i < p->width - out_len - p->precision)
@@ -47,27 +47,28 @@ static void		add_no_minus_flag_output(t_printf *p, int out_len,
 	ft_putstr(out_str);
 }
  
-static __int128	add_choose_di_type(t_printf *p, va_list *ap)
+static void	add_choose_di_type(t_printf *p, va_list *ap, __int128 *out)
 {
-	__int128	out;
-
-	out = 0;
+	*out = 0;
 	if (p->length[0] == 'h' && p->length[1] == 'h')
-		out = (char)va_arg(*ap, int);
+		*out = (char)va_arg(*ap, int);
 	else if (p->length[0] == 'h' && !p->length[1])
-		out = (short)va_arg(*ap, int);
+		*out = (short)va_arg(*ap, int);
 	else if (p->length[0] == 'l' && !p->length[1])
-		out = (long)va_arg(*ap, long);
-	return (out);
+		*out = (long)va_arg(*ap, long);
+	else if (p->length[0] == 'l' && p->length[1] == 'l')
+		*out = (long long)va_arg(*ap, long long);
+	else
+		*out = (int)va_arg(*ap, int);
 }
 
-bool			pf_output_decimal(t_printf *p, va_list *ap)
+bool		pf_output_decimal(t_printf *p, va_list *ap)
 {
 	__int128	out;
 	string		out_str;
-	int			out_len;
+	long		out_len;
 
-	out = add_choose_di_type(p, ap);
+	add_choose_di_type(p, ap, &out);
 	_NOTISM_F(E_ALLOC, out_str = ft_maxitoa(out));
 	out_len = ft_strlen(out_str);
 	if (p->is_precision)
@@ -75,10 +76,8 @@ bool			pf_output_decimal(t_printf *p, va_list *ap)
 		: (p->precision = 0);
 	p->flags[1] ? ++(out_len) : 0;
 	p->counter += out_len;
-	if (p->flags[0])
-		add_is_minus_flag_output(p, out_len, out, out_str);
-	else
-		add_no_minus_flag_output(p, out_len, out, out_str);
+	(p->flags[0]) ? add_is_minus_flag_output(p, out_len, out, out_str)
+	: add_no_minus_flag_output(p, out_len, out, out_str);
 	ft_strdel(&out_str);
 	return (true);
 }
