2de2f51ae19e0ce4d0d699cba93b87c42cd2900e
Iipal <malkoleyplay@gmail.com>
2019-03-12 20:07:45 +0200

    fixed norme & start refactoring %d %i %u %x %X

--- srcs/pf_address.c
+++ srcs/pf_address.c
@@ -6,37 +6,13 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 16:42:34 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/03/12 17:31:48 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/03/12 19:25:26 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static void	add_choose_addr_length(t_printf *p, va_list *ap, intptr_t *addr)
-{
-	*addr = 0;
-	if (p->symbol == 'p')
-		*addr = (intptr_t)va_arg(*ap, void*);
-	else
-	{
-		if (p->length[0] == 'h' && p->length[1] == 'h')
-			*addr = (unsigned char)va_arg(*ap, int);
-		else if (p->length[0] == 'h' && !p->length[1])
-			*addr = (unsigned short)va_arg(*ap, int);
-		else if (p->length[0] == 'l' && !p->length[1])
-			*addr = (unsigned long)va_arg(*ap, unsigned long);
-		else if (p->length[0] == 'l' && p->length[1] == 'l')
-			*addr = (unsigned long long)va_arg(*ap, unsigned long long);
-		else if (p->length[0] == 'z' && !p->length[1])
-			*addr = (size_t)va_arg(*ap, size_t);
-		else if (p->length[0] == 'j' && !p->length[1])
-			*addr = (uintmax_t)va_arg(*ap, uintmax_t);
-		else
-			*addr = (unsigned int)va_arg(*ap, unsigned int);
-	}
-}
-
-static void	add_is_minus_flag_output(t_printf *p, string out, long prec)
+static void	add_is_minus_output(t_printf *p, string out, long prec)
 {
 	long	i;
 	long	out_len;
@@ -53,7 +29,7 @@ static void	add_is_minus_flag_output(t_printf *p, string out, long prec)
 		_PUT(' ');
 }
 
-static void	add_no_minus_flag_output(t_printf *p, string out, long prec)
+static void	add_no_minus_output(t_printf *p, string out, long prec)
 {
 	long	i;
 	long	out_len;
@@ -85,13 +61,15 @@ bool		pf_address(t_printf *p, va_list *ap)
 	long		prec;
 
 	prec = 0;
-	add_choose_addr_length(p, ap, &addr);
-	out = ft_ltoa_base(addr, 16);
+	addr = (intptr_t)va_arg(*ap, void*);
+	_NOTIS_F(out = ft_ltoa_base(addr, 16));
 	ft_strtolower(out);
 	if (p->is_precision)
 		if (p->precision > (int)ft_strlen(out))
 			prec = p->precision - (int)ft_strlen(out);
-	(p->flags[M]) ? add_is_minus_flag_output(p, out, prec) : 0;
-	(p->flags[M]) ? 0 : add_no_minus_flag_output(p, out, prec);
+	(p->flags[M])
+	? add_is_minus_output(p, out, prec)
+	: add_no_minus_output(p, out, prec);
+	ft_strdel(&out);
 	return (true);
 }
--- srcs/pf_decimal.c
+++ srcs/pf_decimal.c
@@ -5,135 +5,43 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/09 15:56:35 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/03/11 23:10:02 by tmaluh           ###   ########.fr       */
+/*   Created: 2019/03/12 19:08:14 by tmaluh            #+#    #+#             */
+/*   Updated: 2019/03/12 19:23:19 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static void	add_is_minus_flag_output(t_printf *p, long out_len,
-										long out, string out_str)
+static void	add_choose_decimal_length(t_printf *p, va_list *ap, __int128_t *o)
 {
-	long	i;
-	long	prec;
-
-	i = out_len - 1;
-	if (p->symbol != 'u')
-	{
-		if (p->is_precision && !p->precision && p->symbol == 'o')
-				;
-		else if (p->flags[P] && out >= 0)
-			_PUT('+');
-		else if (p->flags[P] && out < 0)
-			--(p->counter);
-	}
-	prec = p->precision;
-	i += p->precision;
-	while (prec--)
-		_PUT('0');
-	if (p->is_precision && !p->precision && !out)
-		p->counter -= out_len;
-	else
-		ft_putstr(out_str);
-	while (++i < p->width)
-		p->flags[Z] ? _PUT('0') : _PUT(' ');
-}
-
-static void	add_no_minus_flag_output(t_printf *p, long out_len,
-										long out, string out_str)
-{
-	long	i;
-	long	prec;
-
-	i = -1;
-	while (++i < p->width - out_len - p->precision)
-		p->flags[Z] ? _PUT('0') : _PUT(' ');
-	if (p->symbol != 'u')
-	{
-		if (p->is_precision && !p->precision && p->flags[P] && p->symbol == 'o')
-			;
-		else if (p->is_precision && !p->precision && p->symbol == 'o')
-			_PUT('+');
-		else if (p->flags[P] && out >= 0)
-			_PUT('+');
-		else if (p->flags[P] && out < 0)
-			--(p->counter);
-	}
-	prec = p->precision;
-	while (prec--)
-		_PUT('0');
-	if (p->is_precision && !p->precision && !out)
-	{
-		p->counter -= out_len;
-		if (p->symbol == 'o')
-		{
-			ft_putstr(out_str);
-			p->counter += ft_strlen(out_str);
-		}
-	}
-	else
-		ft_putstr(out_str);
-}
-
-static void	add_choose_di_datatype(t_printf *p, va_list *ap,
-											__int128 *out)
-{
-	*out = 0;
+	*o = 0;
 	if (p->length[0] == 'h' && p->length[1] == 'h')
-		*out = (char)va_arg(*ap, int);
+		*o = (char)va_arg(*ap, int);
 	else if (p->length[0] == 'h' && !p->length[1])
-		*out = (short)va_arg(*ap, int);
+		*o = (short)va_arg(*ap, int);
 	else if (p->length[0] == 'l' && !p->length[1])
-		*out = (long)va_arg(*ap, long);
+		*o = (long)va_arg(*ap, long);
 	else if (p->length[0] == 'l' && p->length[1] == 'l')
-		*out = (long long)va_arg(*ap, long long);
+		*o = (long long)va_arg(*ap, long long);
 	else if (p->length[0] == 'z' && !p->length[1])
-		*out = (intmax_t)va_arg(*ap, intmax_t);
+		*o = (intmax_t)va_arg(*ap, intmax_t);
 	else if (p->length[0] == 'j' && !p->length[1])
-		*out = (size_t)va_arg(*ap, size_t);
+		*o = (size_t)va_arg(*ap, size_t);
 	else
-		*out = (int)va_arg(*ap, int);
-}
-
-static void	add_choose_udatatype(t_printf *p, va_list *ap,
-											__int128 *out)
-{
-	*out = 0;
-	if (p->length[0] == 'h' && p->length[1] == 'h')
-		*out = (unsigned char)va_arg(*ap, int);
-	else if (p->length[0] == 'h' && !p->length[1])
-		*out = (unsigned short)va_arg(*ap, int);
-	else if (p->length[0] == 'l' && !p->length[1])
-		*out = (unsigned long)va_arg(*ap, unsigned long);
-	else if (p->length[0] == 'l' && p->length[1] == 'l')
-		*out = (unsigned long long)va_arg(*ap, unsigned long long);
-	else if (p->length[0] == 'z' && !p->length[1])
-		*out = (size_t)va_arg(*ap, size_t);
-	else if (p->length[0] == 'j' && !p->length[1])
-		*out = (uintmax_t)va_arg(*ap, uintmax_t);
-	else
-		*out = (unsigned int)va_arg(*ap, unsigned int);
+		*o = (int)va_arg(*ap, int);
 }
 
 bool		pf_decimal(t_printf *p, va_list *ap)
 {
-	__int128	out;
+	__int128_t	out;
 	string		out_str;
-	long		out_len;	
+	long		out_len;
+	long		prec;
 
-	ft_is_one_of_n(p->symbol, 2, 'u', 'o')
-	? add_choose_udatatype(p, ap, &out) : add_choose_di_datatype(p, ap, &out);
-	_NOTIS_F(add_choose_out_str(&out_str, p->symbol, out));
+	prec = 0;
+	add_choose_decimal_length(p, ap, &out);
+	out_str = ft_maxitoa(out);
 	out_len = ft_strlen(out_str);
-	(p->flags[4] && !ft_is_one_of_n(p->symbol, 2, 'u', 'o')
-	&& !p->flags[P]) ? _PUT(' ') : 0;
-	(p->is_precision && p->precision > out_len) ? (p->precision -= out_len)
-	: (p->precision = 0);
-	p->flags[P] ? ++(out_len) : 0;
-	p->counter += out_len;
-	(p->flags[M]) ? add_is_minus_flag_output(p, out_len, out, out_str)
-	: add_no_minus_flag_output(p, out_len, out, out_str);
 	ft_strdel(&out_str);
 	return (true);
 }
