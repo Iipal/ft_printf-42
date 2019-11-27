33eb0d18d36775f8396e842f4ee87cdf4de0e74d
Iipal <malkoleyplay@gmail.com>
2019-11-27 13:52:12 +0200

    function select

--- srcs/flags/flags_processing/pf_get_processing_func.c
+++ srcs/flags/flags_processing/pf_get_processing_func.c
@@ -6,27 +6,51 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/23 21:41:17 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/11/25 21:34:48 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/11/27 13:16:06 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_internal.h"
 
-inline bool	pf_get_processing_func(va_list *ap)
+static inline bool __attribute__((__const__))
+	invalid(va_list *ap)
 {
-	bool	ret;
+	return (!ap);
+}
 
-	ret = false;
-	if (g_flag == 'd' || g_flag == 'i')
-		ret = pf_decimal(ap);
-	else if (g_flag == 's' || g_flag == 'c' || g_flag == '%')
-		ret = pf_string(ap);
-	else if (g_flag == 'p')
-		ret = pf_address(ap);
-	else if (g_flag == 'o')
-		ret = pf_octal(ap);
-	else if (g_flag == 'u')
-		ret = pf_unsigned(ap);
+static inline bool __attribute__((__const__))
+	s_run_func(const size_t index, va_list *ap)
+{
+	static bool	(*fn[])(va_list*) = { invalid, pf_decimal, pf_string,
+						pf_address, pf_octal, pf_unsigned, NULL, NULL };
+
+	return (fn[index](ap));
+}
+
+static inline size_t __attribute__((__const__))
+	s_get_func_index(void)
+{
+	static const char	arr[16] = { 'd', 'i', 's', 'c', '%', 'p', 'o', 'u', 0 };
+	ptrdiff_t			i;
+
+	i = 0L;
+	while (g_flag != arr[i])
+		++i;
+	if (2L > i)
+		return (1L);
+	else if (5L > i)
+		return (2L);
+	else if (8L > i)
+		return (i - 2L);
+	else
+		return (0L);
+}
+
+inline bool
+	pf_get_processing_func(va_list *ap)
+{
+	const bool	ret = s_run_func(s_get_func_index(), ap);
+
 	refresh_flag_global_data();
 	if (!ret)
 		ft_putendl_fd(E_PF_INVALID, STDERR_FILENO);
